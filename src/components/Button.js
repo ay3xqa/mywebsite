bf505c34025cdbe7c333031ad25f0ba9d676baed
Yu <ay3xqa@virginia.edu>
2022-02-07 14:08:54 -0500

    update resume and add pm description

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Button.css';
 import { Link } from 'react-router-dom';
-import Pdf from '../components/AlexYuResume.pdf'
+import Pdf from '../components/AlexYu-1-4-22.pdf'
 
 const STYLES = ['btn--primary', 'btn--outline', 'btn--test', 'btn--resume'];
 
