e071daf381963112c34bea33db8f1b59ae302a61
Yu <ay3xqa@virginia.edu>
2024-07-13 21:51:26 -0400

    work changes

--- src/components/Work.js
+++ src/components/Work.js
@@ -5,7 +5,7 @@ export default function Work() {
     return (
         <div className='Work-container'>
       <h1 className='a12'>Work I've Done</h1>
-      <p className='a12'>Check out UFC sports picks here</p>
+      <p className='a12'>Check out UFC sports picks <a href="https://ay3xqa.github.io/myUFCAndPortfolio/">here</a></p>
       </div>
     )
 }
